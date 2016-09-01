151803e700b4c68bbe4d27d0291401abfbac3d60
Peter Caulfield <peterjcaulfield@gmail.com>
2016-09-01 16:39:42 +0100

    configure hot reloading for react

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -35,6 +35,11 @@ var config;
 
 const TARGET = process.env.npm_lifecycle_event;
 
+// set babel env so we can enable hot reloading in react
+// note we still need to patch the redux store
+// https://github.com/survivejs-demos/redux-demo/blob/e84d05d63fdba9eb67c3c391db5c48e71a4c9c6e/kanban_app/app/store/configureStore.dev.js#L20
+process.env.BABEL_ENV = TARGET;
+
 if (TARGET === 'start') {
     config = merge(
         common,
